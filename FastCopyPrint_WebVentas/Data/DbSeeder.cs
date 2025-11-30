6fe9457aec3888f55f2892e2ca2b56f2636e2c7a
Sa1kxd <samildelacruz1@gmail.com>
2025-11-30 16:22:12 -0400

    Dbseeder para insertar un administrador por defecto en la Db

---
+++ FastCopyPrint_WebVentas/Data/DbSeeder.cs
@@ -0,0 +1,46 @@
+﻿using FastCopyPrint_Ventas.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FastCopyPrint_WebVentas.Data;
+
+public static class DbSeeder
+{
+    public static async Task SeedRolesAndAdminAsync(IServiceProvider service)
+    {
+        var userManager = service.GetRequiredService<UserManager<ApplicationUser>>();
+        var roleManager = service.GetRequiredService<RoleManager<IdentityRole>>();
+        var contexto = service.GetService<ApplicationDbContext>();
+
+        await CheckCreateRole(roleManager, "Admin");
+ 
+        var adminEmail = "admin@fastcopy.com";
+        var adminUser = await userManager.FindByEmailAsync(adminEmail);
+        if (adminUser == null)
+        {
+            adminUser = new ApplicationUser
+            {
+                UserName = adminEmail,
+                Email = adminEmail,
+                EmailConfirmed = true, 
+                Nombre = "Administrador Principal",
+                Activo = true,
+                FechaRegistro = DateTime.UtcNow
+            };
+            var result = await userManager.CreateAsync(adminUser, "Admin$1234");
+
+            if (result.Succeeded)
+            {
+                await userManager.AddToRoleAsync(adminUser, "Admin");
+            }
+        }
+    }
+
+    private static async Task CheckCreateRole(RoleManager<IdentityRole> roleManager, string roleName)
+    {
+        if (!await roleManager.RoleExistsAsync(roleName))
+        {
+            await roleManager.CreateAsync(new IdentityRole(roleName));
+        }
+    }
+}
